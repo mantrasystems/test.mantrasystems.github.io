4a15a505eb9d7f385958a9697c9dbda1504a20a0
Mantra Ventures CTO <rico@moscovium.local>
2020-12-05 11:53:55 +0000

    Remove files

--- assets/js/cleaner.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("DOMContentLoaded", function() {
-  function eraseCookie(name) {   
-    document.cookie = name+'=; Max-Age=-99999999; SameSite=Lax';
-  }
-
-  eraseCookie('_ga');
-  eraseCookie('_gat_gtag_UA_150294559_1');
-  eraseCookie('_gid');
-});
